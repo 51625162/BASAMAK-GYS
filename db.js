13a135adb4401743f5f609387089fcf18d8f2289
51625162 <kasim.merkez.02@gmail.com>
2026-08-14 15:34:08 +0300

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-/* BASAMAK-GYS database loader - cache refresh */
-(function(){
-  const v='20260814d';
-  document.write('<script src="db-core.js?v='+v+'"><\\/script>');
-  document.write('<script src="icerik-gorsel-fix.js?v='+v+'"><\\/script>');
-})();
