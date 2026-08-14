669b051e7f6d12bfef341db6f03d37454797dba0
51625162 <kasim.merkez.02@gmail.com>
2026-08-14 15:02:06 +0300

    Fix cross-device cache versioning for database and image fixes

--- db.js
+++ db.js
@@ -1,2 +1,6 @@
-document.write('<script src="db-core.js?v=20260814b"><\/script>');
-document.write('<script src="icerik-gorsel-fix.js?v=20260814b"><\/script>');
+/* BASAMAK-GYS database loader - cache refresh */
+(function(){
+  const v='20260814c';
+  document.write('<script src="db-core.js?v='+v+'"><\\/script>');
+  document.write('<script src="icerik-gorsel-fix.js?v='+v+'"><\\/script>');
+})();
