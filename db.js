45804205437e92fcde110a75475db1a0260ad9b4
51625162 <kasim.merkez.02@gmail.com>
2026-08-14 15:09:47 +0300

    Görsel kayıt katmanı için DB çekirdek önbellek sürümünü yenile

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 /* BASAMAK-GYS database loader - cache refresh */
 (function(){
-  const v='20260814c';
+  const v='20260814d';
   document.write('<script src="db-core.js?v='+v+'"><\\/script>');
   document.write('<script src="icerik-gorsel-fix.js?v='+v+'"><\\/script>');
 })();
